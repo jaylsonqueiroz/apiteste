898e688f9d511a4e21e24254d1a52197e9ddb904
jaylson.queiroz@gmail.com <jaylson.queiroz@gmail.com>
2023-03-21 17:20:15 -0300

    Teste alteracao de codigo

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+'''from flask import Flask, request
 import pdfkit
 
 app = Flask(__name__)
@@ -18,4 +18,36 @@ def convert_html_to_pdf():
     return f'PDF gerado em: {pdf_path}'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True)'''
+
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+# Make the WSGI interface available at the top level so wfastcgi can get it.
+wsgi_app = app.wsgi_app
+
+@app.route('/')
+def home():
+   return 'hello world'   
+
+profissional = [
+    {
+        'id': 1,
+        'nome': u'Luigi Tavolaro',
+        'cargo': u'Desenvolvedor', 
+        'idade': 36
+    },
+    {
+        'id': 2,
+        'nome': u'Douglas Pereira',
+        'cargo': u'RH', 
+        'idade': 25
+    }
+]
+
+@app.route('/api', methods=['GET'])
+def api_profissionais():
+    return jsonify({'profissional': profissional}) 
+
+if __name__ == '__main__':
+    app.run()
